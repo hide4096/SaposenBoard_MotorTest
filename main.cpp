3c28c91141a69ae559b0c629f66332981dd1d8e1
hide4096 <asouhide2002@gmail.com>
2023-05-15 07:56:04 +0000

    [add]ライントレースできた...?

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 #include <math.h>
 
 #define PI 3.141592
-#define I_MAX 10000
-#define IMU_ADRS 0x68
+#define I_MAX 1000
 
 /*
     モータ制御ピン
@@ -37,87 +36,74 @@ AnalogIn adc4(PB_0);
 
 DigitalIn sw(PF_1);
 
-I2C imu(PB_7,PB_6);
-
 Thread run_pid(osPriorityRealtime,1024);
-Thread print_info(osPriorityNormal,1024);
 Semaphore running(1);
 Ticker loop1ms;
 
 bool motor_is_disable = true;
 
-void ControlMotor(float duty_r,float duty_l){
+void ControlMotor(int width_r,int width_l){
     if(motor_is_disable){
-        MOT1_IN1.write(0);
-        MOT1_IN2.write(0);
-        MOT2_IN1.write(0);
-        MOT2_IN2.write(0);
+        MOT1_IN1.pulsewidth_us(0);
+        MOT1_IN2.pulsewidth_us(0);
+        MOT2_IN1.pulsewidth_us(0);
+        MOT2_IN2.pulsewidth_us(0);
         return;
     }
 
-    if(duty_l>0.){
-        MOT1_IN1.write(0);
-        MOT1_IN2.write(duty_l);
+    if(width_r > 1000) width_r = 1000;
+    else if(width_r < -1000) width_r = -1000;
+    if(width_l > 1000) width_l = 1000;
+    else if(width_l < -1000) width_l = -1000;
+
+    if(width_l>0){
+        MOT1_IN1.pulsewidth_us(0);
+        MOT1_IN2.pulsewidth_us(width_l);
     }else{
-        MOT1_IN1.write(-duty_l);
-        MOT1_IN2.write(0);
+        MOT1_IN1.pulsewidth_us(-width_l);
+        MOT1_IN2.pulsewidth_us(0);
     }
-    if(duty_r>0.){
-        MOT2_IN1.write(duty_r);
-        MOT2_IN2.write(0);
+    if(width_r>0){
+        MOT2_IN1.pulsewidth_us(width_r);
+        MOT2_IN2.pulsewidth_us(0);
     }else{
-        MOT2_IN1.write(0);
-        MOT2_IN2.write(-duty_r);
+        MOT2_IN1.pulsewidth_us(0);
+        MOT2_IN2.pulsewidth_us(-width_r);
     }
 }
 
-uint16_t line_buf[4][9];
 uint16_t line_l1,line_l2,line_r1,line_r2;
 
 void FetchLineSensor(){
-    for(int i=0;i<9;i++){
-        line_buf[0][i] = adc1.read_u16();
-        line_buf[1][i] = adc2.read_u16();
-        line_buf[2][i] = adc3.read_u16();
-        line_buf[3][i] = adc4.read_u16();
-    }
-    for(int i=0;i<9;i++){
-        for(int j=9;j>i;j--){
-            for(int k=0;k<4;k++){
-                if(line_buf[k][j-1] > line_buf[k][j]){
-                    uint16_t tmp = line_buf[k][j];
-                    line_buf[k][j] = line_buf[k][j-1];
-                    line_buf[k][j-1] = tmp;
-                }
-            }
-
-        }
-    }
-    line_r2 = line_buf[0][4];
-    line_r1 = line_buf[1][4];
-    line_l1 = line_buf[2][4];
-    line_l2 = line_buf[3][4];
+    line_r2 = adc1.read_u16()>>4;
+    line_r1 = adc2.read_u16()>>4;
+    line_l1 = adc3.read_u16()>>4;
+    line_l2 = adc4.read_u16()>>4;
 }
 
-const float PGain = 0.01,IGain = 0.,DGain = 0.0;
-float speed = 0.;
-int32_t I_diff = 0.,past_diff = 0.;
+//ここの値を書き換える
+const float PGain = 2.0,IGain = 0.005,DGain = 0.1;
+
+uint16_t speed = 0.;
+long I_diff = 0.,past_diff = 0.;
 
 void pid(){
-    float line_avr_r = line_r2 + line_r1;
-    float line_avr_l = line_l2 + line_l1;
+    //左右それぞれ２個のセンサの平均値をとってる
+    //重み付けをかえるといいかも
+    int line_avr_r = (line_r2 + line_r1)/2;
+    int line_avr_l = (line_l2 + line_l1)/2;
 
-    int32_t diff = line_avr_r - line_avr_l;
+    int diff = line_avr_r - line_avr_l;
 
-    float duty_l = speed + (diff*PGain+I_diff*IGain+(diff - past_diff)*DGain)*0.;
-    float duty_r = speed - (diff*PGain+I_diff*IGain+(diff - past_diff)*DGain)*0.;
+    int speed_l = speed + (diff*PGain+I_diff*IGain+(diff - past_diff)*DGain);
+    int speed_r = speed - (diff*PGain+I_diff*IGain+(diff - past_diff)*DGain);
 
     past_diff = diff;
     I_diff+=diff;
     if(I_diff > I_MAX) I_diff = I_MAX;
     else if(I_diff < -I_MAX) I_diff = -I_MAX;
 
-    ControlMotor(duty_r,duty_l);
+    ControlMotor(speed_r,speed_l);
 }
 
 //ライントレースの処理
@@ -129,35 +115,27 @@ void linetrace(){
     }
 }
 
-//ログ出力処理
-void showinfo(){
-    while(1){
-        printf("%d\t%d\t%d\t%d\r\n",line_r2,line_r1,line_l1,line_l2);
-        ThisThread::sleep_for(250);
-    }
-}
-
 void flip1ms(){
     running.release();
 }
 
 int main(){
-    motor_is_disable = false;
-    speed = 0.0;
 
     sw.mode(PullNone);
 
     //モータの設定
-    MOT1_IN1.period_us(100);
-    MOT1_IN2.period_us(100);
-    MOT2_IN1.period_us(100);
-    MOT2_IN2.period_us(100);
+    MOT1_IN1.period_us(1000);
+    MOT1_IN2.period_us(1000);
+    MOT2_IN1.period_us(1000);
+    MOT2_IN2.period_us(1000);
 
     printf("Initialize Complete!\r\n");
 
     while(sw);
 
-    osStatus st_print = print_info.start(showinfo);
+    motor_is_disable = false;
+    speed = 180;
+
     osStatus st_runpid = run_pid.start(linetrace);
     loop1ms.attach_us(&flip1ms,1000);
 
